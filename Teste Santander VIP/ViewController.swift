1381d7eb5e91e15d36f63a0d78caab4a3bf477b4
Filipe <fil.desouza@gmail.com>
2021-09-14 17:21:34 -0300

    Initial Commit

---
+++ Teste Santander VIP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Teste Santander VIP
+//
+//  Created by Virtual Machine on 14/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Teste Santander VIPTests/Teste_Santander_VIPTests.swift
@@ -0,0 +1,33 @@
+//
+//  Teste_Santander_VIPTests.swift
+//  Teste Santander VIPTests
+//
+//  Created by Virtual Machine on 14/09/21.
+//
+
+import XCTest
+@testable import Teste_Santander_VIP
+
+class Teste_Santander_VIPTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
